c28da484cc29caa0232c4ba7c1a2a6a8f6920cb5
laura.souza <laura.souza@gclaims.com.br>
2023-03-13 23:05:34 -0300

    others operations

--- aula_3/src/operations.js
+++ aula_3/src/operations.js
@@ -3,13 +3,57 @@ const TypeVariableConstraint = require('./validate');
 
 const Operations = {
 
-    soma(value_1, value_2) {
+    plus(value_1, value_2) {
         const validateValue_1 = validate({ value_1, value_2 },  TypeVariableConstraint.typeVariableConstraint);
  
         if(validateValue_1 !== undefined){
             return 'Erro! Os valores inseridos não são do tipo number';
         } 
         let soma = value_1 + value_2;
+        return soma;
+        
+    },
+
+    subtraction(value_1, value_2) {
+        const validateValue_1 = validate({ value_1, value_2 },  TypeVariableConstraint.typeVariableConstraint);
+ 
+        if(validateValue_1 !== undefined){
+            return 'Erro! Os valores inseridos não são do tipo number';
+        } 
+        let soma = value_1 - value_2;
+        return soma;
+        
+    },
+
+    division(value_1, value_2) {
+        const validateValue_1 = validate({ value_1, value_2 },  TypeVariableConstraint.typeVariableConstraint);
+ 
+        if(validateValue_1 !== undefined){
+            return 'Erro! Os valores inseridos não são do tipo number';
+        } 
+        let soma = value_1 / value_2;
+        return soma;
+        
+    },
+
+    multiplication(value_1, value_2) {
+        const validateValue_1 = validate({ value_1, value_2 },  TypeVariableConstraint.typeVariableConstraint);
+ 
+        if(validateValue_1 !== undefined){
+            return 'Erro! Os valores inseridos não são do tipo number';
+        } 
+        let soma = value_1 * value_2;
+        return soma;
+        
+    },
+
+    exponentiation(value_1, value_2) {
+        const validateValue_1 = validate({ value_1, value_2 },  TypeVariableConstraint.typeVariableConstraint);
+ 
+        if(validateValue_1 !== undefined){
+            return 'Erro! Os valores inseridos não são do tipo number';
+        } 
+        let soma = value_1**value_2;
         return soma;
         
     }
